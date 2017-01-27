25af068de4e2a164d1a4d55638a48fdce79c494a
jordan piepkow <jpiepkow@gmail.com>
2017-01-27 10:06:59 -0500

    changed the file format to pretty

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 (function() {
-	var recursive = require('recursive-readdir');
-	require('shelljs/global');
-	var base = process.cwd()
-	recursive(base, function (err, files) {
-  		// Files is an array of filename 
-  	var files = files.filter(x => x.endsWith('.js'))
-  		files.forEach(file => {
-			exec(`prettier --write ${file}`)
-  		})
-	});
-})()
+  var recursive = require("recursive-readdir");
+  require("shelljs/global");
+  var base = process.cwd();
+  recursive(base, function(err, files) {
+    // Files is an array of filename
+    var files = files.filter(x => x.endsWith(".js"));
+    files.forEach(file => {
+      exec(`prettier --write ${file}`);
+    });
+  });
+})();

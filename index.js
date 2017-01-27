0b5069a49212083b64602512e18ef86e827dcf54
jordan piepkow <jpiepkow@gmail.com>
2017-01-27 10:29:23 -0500

    changed to single quotes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 (function() {
-  var recursive = require("recursive-readdir");
-  var path = require("path");
-  require("shelljs/global");
+  var recursive = require('recursive-readdir');
+  var path = require('path');
+  require('shelljs/global');
   var base = process.cwd();
   function ignoreFunc(file, stats) {
-    return file.includes("node_modules");
+    return file.includes('node_modules');
   }
   recursive(base, [ ignoreFunc ], function(err, files) {
     // Files is an array of filename
-    var files = files.filter(x => x.endsWith(".js"));
+    var files = files.filter(x => x.endsWith('.js'));
     console.log(files);
     files.forEach(file => {
-      exec(`prettier --write ${file}`);
+      exec(`prettier --write --single-quote  ${file}`);
     });
   });
 })();
